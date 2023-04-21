90bb263ef70804774cc6ca17dae51a5b18725388
Jocelyn-Hn <jocelyn07198@gmail.com>
2023-04-21 13:15:21 -0500

    Updated test_account file

--- test_account.py
+++ test_account.py
@@ -1,3 +1,7 @@
+from pytest import *
+from account import *
+
+
 class Test:
     def setup_method(self):
         self.a1 = Account('John')
@@ -6,12 +10,32 @@ class Test:
         del self.a1
 
     def test_init(self):
-        pass
+        assert self.a1.get_name() == 'John'
+        assert self.a1.get_balance() == 0
 
     def test_deposit(self):
         # negative, zero, positive
-        pass
+        assert self.a1.deposit(-1.5) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 0
+
+        assert self.a1.deposit(1.5) is True
+        assert self.a1.get_balance() == approx(1.5, abs=0.01)
 
     def test_withdraw(self):
         # negative, zero, positive invalid, positive valid
-        pass
+        self.a1.deposit(25)
+        assert self.a1.withdraw(25) is True
+        assert self.a1.get_balance() == 0
+
+        self.a1.deposit(15)
+        assert self.a1.withdraw(25) is False
+        assert self.a1.get_balance() == 15
+
+        assert self.a1.withdraw(-1.5) is False
+        assert self.a1.get_balance() == 15
+
+        assert self.a1.deposit(0) is False
+        assert self.a1.get_balance() == 15
